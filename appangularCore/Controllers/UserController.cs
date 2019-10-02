d851ee435047050f1c93ed889eb2a049639c171c
Nacho <nachocriado9@gmail.com>
2019-10-02 02:44:45 -0300

    api terminada para formulario simp´le con swager

---
+++ appangularCore/Controllers/UserController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using appangularCore.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace appangularCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+       
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult Get(int id)
+        {
+            return Ok(new
+            {
+                Nombre = "Charly",
+                Apellido = "Perez",
+                Email = "prueba@netcore.com"
+            });
+        }
+
+
+        [HttpPost("adduser")]
+        public ActionResult Adduser([FromBody] UserViewModel user)
+        {
+            return Ok();
+            
+        }
+
+        
+
+
+    }
+}
---
+++ appangularCore/Models/UserViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace appangularCore.Models
+{
+    public class UserViewModel
+    {
+        public string Nombre { get; set; }
+        public string Apellido { get; set; }
+        public string Email { get; set; }
+
+    }
+}
